1166a51e2c43b15217e365c7680f18985d594c57
ch1n3du <danielonyesoh@gmail.com>
2023-07-24 15:05:10 +0100

    chore: Updated

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,61 @@
 #![no_std]
 #![no_main]
+// Setup custom testing
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-static SIC_MUNDUS: &[u8] = b"Sic Mundus Creatus Est";
 
 // Entry point for the program
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("'ullo governer");
 
-    // Prints "Hello World!" to the VGA buffer.
-    for (i, &byte) in SIC_MUNDUS.iter().enumerate() {
-        unsafe {
-            // Write character byte then the byte for the color black
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
-// Called on panic.
+// Called on a panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test()
+    }
+    exit_qemu(QemuExitCode::Success)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // Make a port for the isa-debug-exit device.
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,182 @@
+use core::fmt::{self, Write};
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+const BUFFER_ADDRESS: usize = 0xb8000;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Green, Color::Black),
+        buffer: unsafe { &mut *(BUFFER_ADDRESS as *mut Buffer) },
+    });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    // use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+/// # The VGA Text Buffer
+/// To print a character to screen in VGA text mode,
+/// one has to write it to the text buffer of the VGA
+/// hardware. The VGA text buffer is a two-dimensional
+/// array with typically 25 rows and 80 colums, which
+/// is directly rendered to the screen. Each array entry
+/// describes a single screen character through the following
+/// format:
+///
+/// | Bit(s) | Value            |
+/// | :--:   | :--:             |
+/// | 0-7    | ASCII code point |
+/// | 8-11   | Foreground color |
+/// | 12-14  | Background color |
+/// | 15     | Blink            |
+///
+/// The writer will always write to the last line and
+/// shift lines up when a line is full (or on `\n`).
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+#[repr(transparent)]
+struct Buffer {
+    // We use volatile to tell the compiler not to optimise this away.
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // Printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Not part of the printable ASCII range.
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code: self.color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        // Move every line up by row and start at the
+        // second highest line.
+        // self.write_str("Called new line");
+
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+/// Represents the background and foreground color of a character.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    const fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+// pub fn print_something() {
+//     let mut writer = Writer {
+//         column_position: 0,
+//         color_code: ColorCode::new(Color::Green, Color::Black),
+//         buffer: unsafe { &mut *(BUFFER_ADDRESS as *mut Buffer) },
+//     };
+
+//     writer.write_byte(b'H');
+//     writer.write_string("ello ");
+//     writer.write_string("Wörld!: \n");
+//     write!(writer, "My name is Daniel").unwrap();
+//     write!(writer, "\nUnneccessary line").unwrap();
+// }
