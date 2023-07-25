d5bef476603c45e964d6934a2dff19d52c90a861
ch1n3du <danielonyesoh@gmail.com>
2023-07-25 19:42:13 +0100

    feat: Finished chapter 4

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -180,3 +180,25 @@ struct ScreenChar {
 //     write!(writer, "My name is Daniel").unwrap();
 //     write!(writer, "\nUnneccessary line").unwrap();
 // }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output")
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Simple single line string";
+    println!("{s}");
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c)
+    }
+}
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,40 +1,19 @@
 #![no_std]
 #![no_main]
-// Setup custom testing
 #![feature(custom_test_frameworks)]
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
-use blog_os::println;
-
-// Entry point for the program
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("'ullo governer");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-// Called on a panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
 }
